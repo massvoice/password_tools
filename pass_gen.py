2feac96fe544f176eaa5d9fd95566844321cb49e
massvoice <chaitanya.citupes@gmail.com>
2016-05-01 14:00:10 +0530

    Password Generation Script
    This is a python script which can be used to generate simple and complex
    passwords based upon the user's requirements.

---
+++ pass_gen.py
@@ -0,0 +1,130 @@
+import random
+
+lower_case_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+upper_case_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numericals_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+special_char_list = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '{', '}']
+
+special_char = []
+numericals = []
+upper_case = []
+lower_case = []
+simple_pass = []
+complex_pass = []
+extra_bit = []
+temp = []
+simple_pass_generated = ""
+complex_pass_generated = ""
+
+class password_generation:
+	def __init__(self):
+		return None
+# Function to generate simple password - assuming that simple passwords cosists of just lower case and upper case characters
+	def simple_password(self,pass_length):
+		count = int(pass_length/2)
+		for i in range(0 , count):
+			simple_pass.append(random.choice(lower_case_list))		
+			simple_pass.append(random.choice(upper_case_list))			
+		mod = pass_length%2
+		if mod > 0:
+			temp =  self.extra_char(mod) + simple_pass
+			random.shuffle(temp)
+			simple_password = "".join(temp)
+			return simple_password
+		else:
+			return "".join(simple_pass)
+			
+# Function to generate complex passwords - these consists of lower case, upper case, numerics as well as special characters
+	def complex_password(self,pass_length):
+		count = int(pass_length/4)
+		for i in range(0 , count):
+			complex_pass.append(random.choice(lower_case_list))		
+			complex_pass.append(random.choice(upper_case_list))		
+			complex_pass.append(random.choice(numericals_list))
+			complex_pass.append(random.choice(special_char_list))
+		mod = pass_length%4
+		if mod > 0:
+			temp =  self.extra_char(mod) + complex_pass
+			random.shuffle(temp)
+			complex_password = "".join(temp)
+			return complex_password
+		else:
+			return "".join(complex_pass)
+
+	def extra_char(self,mod_legth):
+		i=1
+		while i<=mod_legth:
+			if i == 1:
+				extra_bit.append(random.choice(lower_case_list))
+			elif i == 2:
+				extra_bit.append(random.choice(upper_case_list))
+			elif i ==3:
+				extra_bit.append(random.choice(numericals_list))
+			i+=1
+		return extra_bit
+
+# Fuction to generate custom pasword based upon the user's requirement
+
+	def custom_password_generation(self):
+		pass_type = int(raw_input("Which type of password you wish to have? \nPress 1 for Simple \nPress 2 for Complex\n> "))
+		char_choice = int(raw_input("\nPress 1 If you wish to specify the type and number of characters you want \nPress 2 If you wish the computer to do it for you\n> "))
+
+		if char_choice == 2:
+			pass_length = int(raw_input("\nPlease specify the password length ( between 8-20)\n> "))
+			if pass_type == 1 and pass_length in range (8,20):
+				simple_pass_generated =  self.simple_password(int(pass_length))
+				print (simple_pass_generated)
+				
+			elif pass_type == 2 and pass_length in range (8,20):
+				print (self.complex_password(int(pass_length)))
+			else:
+				print ("Wrong Input!!")
+
+		elif char_choice == 1 and pass_type == 1:
+			user_lower = int(raw_input("Please enter the number of lower case characters\n> "))
+			user_upper = int(raw_input("Please enter the number of upper case characters\n> "))
+			for i in range (0, user_lower):
+				lower_case.append(random.choice(lower_case_list))
+			for i in range (0, user_upper):
+				upper_case.append(random.choice(upper_case_list))
+
+			temp = lower_case + upper_case
+			random.shuffle(temp)
+			simple_pass = "".join(temp)
+			print (simple_pass)
+
+		elif char_choice == 1 and pass_type == 2:
+			user_lower = int(raw_input("Please enter the number of lower case characters\n> "))
+			user_upper = int(raw_input("Please enter the number of upper case characters\n> "))
+			user_numeric = int(raw_input("Please enter the number of numeric characters\n> "))
+			user_special_char = int(raw_input("Please enter the number of special characters\n> "))
+			for i in range (0, user_lower):
+				lower_case.append(random.choice(lower_case_list))
+			for i in range (0, user_upper):
+				upper_case.append(random.choice(upper_case_list))
+			for i in range (0, user_numeric):
+				numericals.append(random.choice(numericals_list))
+			for i in range (0, user_special_char):
+				special_char.append(random.choice(special_char_list))
+
+			temp = lower_case + upper_case + numericals + special_char
+			random.shuffle(temp)
+			complex_pass = "".join(temp)
+			print (complex_pass)
+
+# Automatically generate password for the user, takes two parameters: pass_type(can be either simple or complex) and length
+	def automated_pass_generation(self,pass_type,length=8):
+		
+		if length < 8:
+			length = 8
+			print ("Minimum Length required is 8")
+		if pass_type == "simple":
+			return self.simple_password(length)
+		elif pass_type == "complex":
+			return self.complex_password(length)
+		else:
+			return "Wrong Input"
+
+
+
+passGen = password_generation()
